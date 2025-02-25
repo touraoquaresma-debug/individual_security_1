a56888f769eef80770a656fcc58ce0c0995ce376
João Natividade <87316339+joaosnet@users.noreply.github.com>
2025-02-25 12:04:55 -0300

    Atualizar configuração de sparse-checkout para incluir arquivos específicos e corrigir caminho do arquivo de configuração do dataset

--- constants.py
+++ constants.py
@@ -16,7 +16,7 @@ DURACAO_PADRAO = 300  # 5 minutos em segundos
 ARQUIVO_PESOS = str(BASE_DIR / r'runs\pose\train\weights\best.pt')
 ARQUIVO_VIDEO_PADRAO = str(BASE_DIR / 'video_idoso.mp4')
 ARQUIVO_CONFIGURACAO_DATASET = str(
-    BASE_DIR / 'downloads/YOLOElderlyPose.v3i.yolov11/data.yaml'
+    BASE_DIR / 'downloads/YOLOElderlyPose.v2i.yolov11/data.yaml'
 )
 # Estados de pose inicial
 POSE_NAO_DETECTADA = 'não detectado'
